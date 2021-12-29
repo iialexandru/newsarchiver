fce127f437df12b0d919ae60b8c6103a659526cb
Fiils <ioanalexandruipatov12@gmail.com>
2021-12-29 23:45:50 +0200

    libre baskerville font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,35 @@
 import '../styles/scss/globals.scss'
 import Nav from '../components/Nav'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/na.ico" />
+        <link
+            rel="preload"
+            href="/fonts/LibreBaskerville/LibreBaskerville-Regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/LibreBaskerville/LibreBaskerville-Regular.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+            <link
+            rel="preload"
+            href="/fonts/LibreBaskerville/LibreBaskerville-Regular.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </>
